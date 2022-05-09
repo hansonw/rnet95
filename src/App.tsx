942ac0099c2eb5c9e760f4939e3a0f1eb4833d3d
hansonw <hansonw@fb.com>
2022-05-08 21:56:57 -0700

    initial UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {Button, TextField, Window, WindowContent, WindowHeader} from 'react95';
 
 import RNet from './rnet-pi/rnet';
 
 function App() {
-  const [rnetState, setRNetState] = useState('Connecting...');
+  const [url, setURL] = useState(localStorage.getItem('lastUrl') || 'localhost:8080');
+  const [rnetState, setRNetState] = useState('Ready');
+  const rnetRef = useRef<RNet | null>(null);
 
-  useEffect(() => {
-    const rnet = new RNet('localhost:8080');
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setURL(e.target.value);
+    localStorage.setItem('lastUrl', e.target.value);
+  }
+
+  function onConnect() {
+    const rnet = new RNet(url);
+    setRNetState('Connecting...');
     rnet.on('error', err => {
-      console.log(err);
+      console.error('Websocket error:', err);
     });
     rnet.on('connected', () => {
       setRNetState('Connected');
@@ -16,13 +25,30 @@ function App() {
     rnet.on('disconnected', () => {
       setRNetState('Disconnected');
     });
-    return () => rnet.disconnect();
+    rnetRef.current = rnet;
+  }
+
+  useEffect(() => {
+    return () => rnetRef.current?.disconnect();
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">{rnetState}</header>
-    </div>
+    <Window className="window">
+      <WindowHeader
+        style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}
+      >
+        <span>RNet</span>
+      </WindowHeader>
+      <WindowContent>
+        <div style={{display: 'flex'}}>
+          <TextField value={url} onChange={onChange} fullWidth />
+          <Button onClick={onConnect} style={{marginLeft: 4}}>
+            Connect
+          </Button>
+        </div>
+        {rnetState}
+      </WindowContent>
+    </Window>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {createGlobalStyle, ThemeProvider} from 'styled-components';
+import {styleReset} from 'react95';
+// @ts-ignore
+import original from 'react95/dist/themes/original';
+// @ts-ignore
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
+// @ts-ignore
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
 import App from './App';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body {
+    font-family: 'ms_sans_serif';
+  }
+  ${styleReset}
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <ThemeProvider theme={original}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
